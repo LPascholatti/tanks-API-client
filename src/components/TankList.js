f7106ce86ea69de790c0c59c80590d7f5f1bccb8
LPascholatti <lucaspascholatti@gmail.com>
2020-01-14 17:09:39 +0100

    Centralized state in value and removed the other ones, for the filter

--- src/components/TankList.js
+++ src/components/TankList.js
@@ -5,10 +5,6 @@ export default class TankList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      country: "",
-      type: "",
-      ammunition: "",
       value: ""
     };
 
