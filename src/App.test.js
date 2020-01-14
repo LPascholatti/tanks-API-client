646036e731e6888888626cdb4f0dac620c2a934a
LPascholatti <lucaspascholatti@gmail.com>
2020-01-14 22:55:36 +0100

    Implement filter by ammo

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-
-// test("renders learn react link", () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
